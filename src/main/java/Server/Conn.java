6d43376fa8a6a58a3339f9359fff229104b02ce4
Dev Sharma <70541608+devshrm@users.noreply.github.com>
2020-11-09 18:54:36 +0530

    Update Conn.java

--- src/main/java/Server/Conn.java
+++ src/main/java/Server/Conn.java
@@ -10,7 +10,7 @@ public class Conn {
     public Conn() {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
-            c = DriverManager.getConnection("jdbc:mysql://localhost:3306/FoodPile", "devshrm", "3377");
+            c = DriverManager.getConnection("jdbc:mysql://localhost:3306/FoodPile", "", "");
             s = c.createStatement();
             System.out.println("Connected to db");
         } catch (ClassNotFoundException | SQLException e) {
